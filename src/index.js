7d2a8165a8d7f12524e617a8af23266a4a57564d
rudy0628 <s3352250zz@gamil.com>
2022-03-08 21:14:25 +0800

    transform web app to PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './store/index';
 import './index.scss';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
 	<BrowserRouter>
@@ -14,3 +15,8 @@ ReactDOM.render(
 	</BrowserRouter>,
 	document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
